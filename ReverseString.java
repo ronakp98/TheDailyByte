b37282df2f4a4b6b00bab6da7379fe79484ed6bc
ronakp98 <ronak1998@gmail.com>
2021-07-03 22:40:04 -0400

    Initial commit

---
+++ ReverseString.java
@@ -0,0 +1,26 @@
+public class ReverseString
+{
+  public static void main(String args[])
+  {
+    //Intiantiate test strings given on coding prompt
+    String test1 = "Cat";
+    String test2 = "The Daily Byte";
+    String test3 = "civic";
+
+    //Placeholder string to test different cases
+    String str = test2;
+
+    revString(test1);
+    revString(test2);
+    revString(test3);
+
+  }
+
+  public static void revString(String str){
+    for(int i = str.length() - 1; i >= 0; i--)
+    {
+      System.out.print(str.charAt(i));
+    }
+    System.out.print("\n");
+  }
+}
