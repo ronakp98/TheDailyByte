1abc65eca4d7640a08d091504a2c652e5c1d22d3
ronakp98 <ronak1998@gmail.com>
2021-09-02 18:11:33 -0400

    Completed CorrectCapitilization.java
    Finished Challenge #4

--- CorrectCapitilization.java
+++ CorrectCapitilization.java
@@ -10,6 +10,36 @@ return false
 public class CorrectCapitilization {
     public static void main(String args[])
     {
+        //Test Strings
+        String str1 = "USA"; //should return true
+        String str2 = "Calvin"; //should return true
+        String str3 = "compUter"; //should return false
+        String str4 = "coding"; //should return true
+        String str5 = "ComPuTeR";
 
+
+        //Printing test results
+        System.out.println(checkCapitilization(str1));
+        System.out.println(checkCapitilization(str2));
+        System.out.println(checkCapitilization(str3));
+        System.out.println(checkCapitilization(str4));
+        System.out.println(checkCapitilization(str5));
+
+    }
+
+    public static boolean checkCapitilization(String str)
+    {
+        //Return true if entire string is all Capital
+        if(str.equals(str.toUpperCase()))
+        {
+            return true;
+        }
+        //Return true if everything after first letter is lowercase since first letter can either be upper or lower case
+        if(str.substring(1).equals(str.substring(1).toLowerCase()))
+        {
+            return true;
+        }
+        //return false otherwise
+        return false;
     }
 }
