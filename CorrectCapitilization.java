704ed860a9278f03bd1b4542dffeadf44ee88cb6
ronakp98 <ronak1998@gmail.com>
2021-09-02 17:17:19 -0400

    Create CorrectCapitilization.java
    Fourth challenge day

---
+++ CorrectCapitilization.java
@@ -0,0 +1,15 @@
+/*
+Author: Ronak Patel
+
+Correct Capitilization challenge questions to determine if any given string
+is correctly using capitals(first letter, all letters, or no letters). Otherwise
+return false
+
+*/
+
+public class CorrectCapitilization {
+    public static void main(String args[])
+    {
+
+    }
+}
