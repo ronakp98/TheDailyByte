6881daacd04415f23c84c0933ff90c410381d349
ronakp98 <ronak1998@gmail.com>
2021-09-13 18:05:51 -0400

    Update LongestCommonPrefix.java

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -2,7 +2,6 @@
 Author: Ronak Patel
 
 
-
 */
 public class LongestCommonPrefix {
     
@@ -11,24 +10,55 @@ public class LongestCommonPrefix {
         String[] array2 = {"a", "b", "c"};
         String[] array3 = {"spot", "spotty", "spotted"};
 
-
+        System.out.println(commonPrefix(array1));
+        System.out.println(commonPrefix(array2));
+        System.out.println(commonPrefix(array3));
     }
 
-    public String commonPrefix(String[] inArray)
+    public static String commonPrefix(String[] inArray)
     {
-        String returnString = "";
-        boolean isSameFlag = false;
+        int maxLength = smallest(inArray).length();
+        int substring = 0;
+        boolean isSameFlag = true;
+        char compareChar;
 
-        for(int i = 0; i < inArray.length; i++)
+        for(int i = 0; i < maxLength; i++)
         {
-            for(int j = 0; j < inArray[i].length(); j++)
+            compareChar = inArray[0].charAt(i);
+            for (int j = 0; j < inArray.length; j++)
             {
-                
+                if(inArray[j].charAt(i) == compareChar)
+                {
+                    isSameFlag = true;
+                }
+                else{
+                    isSameFlag = false;
+                    break;
+                }
+            }
+            if(isSameFlag == true)
+            {
+                substring++;
+            }
+            else{
+                break;
             }
         }
+        return (inArray[0].substring(0, substring));
+    }
 
-
-        return "hi";
+    
+    public static String smallest(String words[]) {
+        if (words == null || words.length < 1) {
+            return "";
+        }
+        String smallest = words[0];
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].length() < smallest.length()) {
+                smallest = words[i];
+            }
+        }
+        return smallest;
     }
 
     
