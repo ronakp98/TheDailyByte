1bbb85c958b61e55e7584681d61491ba4722f7d2
ronakp98 <ronak1998@gmail.com>
2021-09-13 16:52:32 -0400

    Create LongestCommonPrefix.java

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,35 @@
+/*
+Author: Ronak Patel
+
+
+
+*/
+public class LongestCommonPrefix {
+    
+    public static void main(String[] args) {
+        String[] array1 = {"colorado", "color", "cold"};
+        String[] array2 = {"a", "b", "c"};
+        String[] array3 = {"spot", "spotty", "spotted"};
+
+
+    }
+
+    public String commonPrefix(String[] inArray)
+    {
+        String returnString = "";
+        boolean isSameFlag = false;
+
+        for(int i = 0; i < inArray.length; i++)
+        {
+            for(int j = 0; j < inArray[i].length(); j++)
+            {
+                
+            }
+        }
+
+
+        return "hi";
+    }
+
+    
+}
