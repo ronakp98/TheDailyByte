c07854bf8c9171464333a9438816b98964456daf
ronakp98 <ronak1998@gmail.com>
2021-09-18 00:45:38 -0400

    Update PalindromeRemoval.java

--- PalindromeRemoval.java
+++ PalindromeRemoval.java
@@ -6,11 +6,35 @@ public class PalindromeRemoval {
         String str2 = "foobof";
         String str3 = "abccab";
 
-
+        System.out.println(palindromeWithRemoval(str1));
+        System.out.println(palindromeWithRemoval(str2));
+        System.out.println(palindromeWithRemoval(str3));
     }
 
     public static boolean palindromeWithRemoval(String str)
     {
-        
+
+        StringBuilder inStr = new StringBuilder(str);
+        String removedString = "";
+        String reversedString = "";
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            inStr.setLength(0);
+            inStr.append(str);
+            removedString = inStr.deleteCharAt(i).toString();
+            for(int j = removedString.length() - 1; j >= 0; j--)
+            {
+                reversedString += removedString.charAt(j);
+            }
+            if(reversedString.equals(removedString))
+            {
+                return true;
+            }
+            reversedString = "";
+        }
+
+
+        return false;
     }
 }
